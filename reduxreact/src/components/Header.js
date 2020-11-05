dbaca5bc24ceb859afcea1ccd72537d46ed417fb
Alison Manning <alisonlaurenmanning@gmail.com>
2020-11-04 21:47:59 -0600

    add

---
+++ reduxreact/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>How To Get A Job</h1>
+        </div>
+    )
+}
+
+export default Header;
